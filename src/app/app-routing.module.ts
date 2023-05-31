8d38a1f335b14308d25aa25056ed77fda1131b1d
SUDHAKAR1202 <sudzcrazy94@gmail.com>
2023-05-31 08:48:11 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ListBooksComponent } from './library/list-books/list-books.component';
+import { CreateBooksComponent } from './library/create-books/create-books.component';
+
+
+
+const routes: Routes = [
+ 
+  { path: 'books', component: ListBooksComponent },
+  { path: 'addbook', component: CreateBooksComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
